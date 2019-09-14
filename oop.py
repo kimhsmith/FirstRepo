ae24f29af8ae874f2cb06fbf7bfdd194a8466059
kimhsmith <20kimsmik02@gmail.com>
2019-09-14 22:56:40 +1000

    Third video of Corey Schafer's python tutorial series on OOP

--- oop.py
+++ oop.py
@@ -1,6 +1,9 @@
 # Python Object-Oriented Programming
 
 
+import datetime
+
+
 class Employee:
 
     # Class variables
@@ -23,14 +26,38 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * Employee.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
 
-print(Employee.num_of_emps)
+    # This is basically an alternative constructor.
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+# This implicitly calls __init__()
 emp_1 = Employee('Kim', 'Smith', 50000)
 emp_2 = Employee('John', 'Smyth', 20000)
-print(Employee.num_of_emps)
 
-print(Employee.__dict__)
-print(emp_1.__dict__)
-emp_1.raise_amount = 1.05
-# raise_amount has been added to emp_1 as an attribute, but only for this instance of Employee
-print(emp_1.__dict__)
+Employee.set_raise_amt(1.05)
+
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
+
+emp_str_1 = 'Dylan-Sutton-30000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+print(new_emp_1.email)
+
+my_date = datetime.date(2016, 7, 11)
+
+print(Employee.is_workday(my_date))

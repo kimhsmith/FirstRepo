7bfee0f711aed9e3e517912a1818c29dfffeb376
kimhsmith <20kimsmik02@gmail.com>
2019-09-15 20:05:02 +1000

    Sixth video of Corey Schafer's python tutorial series on OOP

--- oop.py
+++ oop.py
@@ -6,31 +6,38 @@ class Employee:
     # Class variables
     raise_amt = 1.04
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         # These are class attributes
         self.first = first
         self.last = last
-        self.pay = pay
-        self.email = first + '.' + last + '@email.com'
 
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
 
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
-
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.email)
-
-    def __add__(self, other):
-        return self.pay + other.pay
+    @fullname.deleter
+    def fullname(self):
+        print('Delete name!')
+        self.first = None
+        self.last = None
 
 
 # This implicitly calls __init__()
-emp_1 = Employee('Kim', 'Smith', 50000)
-emp_2 = Employee('John', 'Smyth', 20000)
+emp_1 = Employee('Kim', 'Smith')
 
-print(emp_1 + emp_2)
+emp_1.fullname = 'Dylan Sutton'
+
+print(emp_1.email)
+print(emp_1.fullname)
+
+del emp_1.fullname
